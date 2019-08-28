f0bda66302ca6bd6d7921946360ef06146318306
ssvictoro@SALUD.GOB.AR <ssvictoro@SALUD.GOB.AR>
2019-08-28 17:54:16 -0300

    Servicio para traer peliculas

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  constructor(private http: HttpClient) { }
+
+  getFeature() {
+    // tslint:disable-next-line: max-line-length
+    return this.http.get(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22`);
+  }
+}
