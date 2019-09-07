ea1f014fd0e9a4f879c2ce0093b0cf70cba20835
Victor <ojedavd@gmail.com>
2019-09-07 13:30:04 -0300

    Mostrar pantalla de favoritos

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { RespuestaMDB, PeliculaDetalle, RespuestaCredits } from '../interfaces/interfaces';
+import { RespuestaMDB, PeliculaDetalle, RespuestaCredits, Genre } from '../interfaces/interfaces';
 import { environment } from '../../environments/environment';
 
 const URL    = environment.url;
@@ -12,6 +12,7 @@ const apiKey = environment.apiKey;
 export class MoviesService {
 
   private popularesPage = 0;
+  generos: Genre[] = [];
 
   constructor(private http: HttpClient) { }
 
@@ -67,4 +68,20 @@ export class MoviesService {
   getActoresPelicula(id: string) {
     return this.ejecutarQuery<RespuestaCredits>(`/movie/${id}/credits?a=1`);
   }
+
+  cargarGeneros(): Promise<Genre[]> {
+
+    return new Promise( resolve => {
+
+      this.ejecutarQuery(`/genre/movie/list?a=1`)
+        .subscribe( resp => {
+          this.generos = resp['genres'];
+          console.log(this.generos);
+          resolve(this.generos);
+        });
+
+    });
+
+  }
+
 }
