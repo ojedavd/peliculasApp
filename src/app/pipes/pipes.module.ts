edbb224b5f9011091140e7be92895f81b7588c3d
Victor <ojedavd@gmail.com>
2019-09-07 13:58:08 -0300

    Pipe filtro imagen

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImagenPipe } from './imagen.pipe';
 import { ParesPipe } from './pares.pipe';
+import { FiltroImagenPipe } from './filtro-imagen.pipe';
 
 
 
 @NgModule({
-  declarations: [ImagenPipe, 
-    ParesPipe
+  declarations: [
+    ImagenPipe,
+    ParesPipe,
+    FiltroImagenPipe
   ],
   exports: [
     ImagenPipe,
-    ParesPipe
+    ParesPipe,
+    FiltroImagenPipe
   ],
   imports: [
     CommonModule
